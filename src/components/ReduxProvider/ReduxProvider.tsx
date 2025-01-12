4385510ece482f1f526f49c4f64f865f5528d67b
Ahmed Refai <momo3956248@gmail.com>
2025-01-12 17:57:16 +0200

    Updated store

--- src/components/ReduxProvider/ReduxProvider.tsx
+++ src/components/ReduxProvider/ReduxProvider.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import myStore from '@/store/store';
+import { store } from '@/store/store';
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 
 export default function ReduxProvider({ children }: { children: ReactNode }) {
-  return <Provider store={myStore}>{children}</Provider>;
+  return <Provider store={store}>{children}</Provider>;
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,13 +2,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/user.slice';
 //* return a sotre with the reducers
-export const myStore = configureStore({
+export const store = configureStore({
   reducer: {
     userReducer,
   },
 });
 
-type AppStore = typeof myStore;
+type AppStore = typeof store;
 
 export type RootState = ReturnType<AppStore['getState']>;
 // export type dispatchState = ReturnType<(typeof myStore)['dispatch']>;
