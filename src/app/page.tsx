3c823f64db5786871a17b4a05ef4d0e95f352c20
Ahmed Refai <momo3956248@gmail.com>
2025-01-17 22:22:44 +0200

    post Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import PostCard from '@/components/Post/Post';
+import { Container, Grid2 } from '@mui/material';
+
 export default function Home() {
-  return <h1>home</h1>;
+  //* Grid Contianer
+  return (
+    <>
+      <Container>
+        <Grid2 container>
+          <Grid2 size={3}></Grid2>
+          <Grid2 size={6}>
+            <PostCard />
+          </Grid2>
+          <Grid2 size={3}></Grid2>
+        </Grid2>
+      </Container>
+    </>
+  );
 }
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,3 +1,100 @@
-export default function Post() {
-  return <div>Post</div>;
+'use client';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import CommentIcon from '@mui/icons-material/Comment';
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme }) => ({
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: {
+        transform: 'rotate(0deg)',
+      },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: {
+        transform: 'rotate(180deg)',
+      },
+    },
+  ],
+}));
+
+export default function Po() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ width: '100%', p: '10px' }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardContent>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardMedia
+        component="img"
+        image="https://avatars.githubusercontent.com/u/110742430?s=96&v=4"
+        alt="Paella dish"
+      />
+      <CardActions
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          gap: '20px',
+        }}
+      >
+        <IconButton aria-label="add to favorites">
+          <ThumbUpIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <CommentIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
 }
